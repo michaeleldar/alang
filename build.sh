a05d22025b9e248c1caba59759bf82bfec5b9450
michael <halpinm2010@gmail.com>
2021-07-08 16:35:06 +0100

    tried to fix build

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-echo "/home/$USER/alang1/alang_commands/alang.py" >> /home/$USER/.local/bin/alang1
+echo "cd /home/$USER/alang1/alang_commands" > /home/$USER/.local/bin/alang1
+echo "python3 alang.py" > /home/$USER/.local/bin/alang1
