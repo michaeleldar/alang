cd1b2ebf5374b2a278d1b8377694c0c2410752f0
michael <halpinm2010@gmail.com>
2021-07-08 12:45:30 +0100

    moved file

--- alang_commands/alang.py
+++ alang_commands/alang.py
@@ -21,27 +21,6 @@ indinto.write(indent)
 commands = [writeala, addala, comala, declareala]
 
 
-def subin():  # takes away a indent
-    global indent
-    if indent == "":
-        print(
-            "ERROR, END CANNOT BE USED WITHOUT A PRECEEDING IF, ELSE, ELSE_IF, WHILE, DEF_FUNC OR REPEAT :(")
-        outfileo.truncate()
-        quit()
-    elif indent == "    ":
-        indent = ""
-    elif indent == "        ":
-        indent = "    "
-    elif indent == "            ":
-        indent = "        "
-    elif indent == "                ":
-        indent = "        "
-    else:
-        print("ERROR, TOO MANY NESTED LOOPS :(")
-        outfileo.truncate()
-        quit()
-
-
 def scan(line):  # Main compiler
     for command in commands:
         if command.statement == line[0]:
