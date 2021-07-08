601b89169a26b58f184c28f91179aa2fbf7e0567
michael <halpinm2010@gmail.com>
2021-07-08 10:37:45 +0100

    h

--- alang_commands/write.py
+++ alang_commands/write.py
@@ -1,6 +1,9 @@
 statement = "WRITE"
 
-infilet = open('/home/michael/alang/global_vars/infile')
+infilet = open('/home/$USER/alang/global_vars/infile', 'r')
+outfilet = open('/home/$USER/alang/global_vars/outfile', 'r')
+indent = open('/home/$USER/alang/global_vars/indent', 'r')
+infileo = open(infilet.read(), 'w')
 
 
 def translate():
